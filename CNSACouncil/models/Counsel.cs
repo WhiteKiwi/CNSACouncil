c2a11c886913e022747f498931d5d4dcc33fcf09
장지훈 <s5v7@outlook.kr>
2018-07-23 11:08:31 +0900

    Counsel Model 추가

---
+++ CNSACouncil/models/Counsel.cs
@@ -0,0 +1,20 @@
+﻿namespace CNSACouncil.Models {
+	/// <summary>
+	/// Model of agrees table
+	/// </summary>
+	public class Counsel {
+		public int ID { get; set; } // 상담 일련번호
+
+		public string OfficerID { get; set; } // 학생회 임원 아이디
+
+		public string Date { get; set; } // 원하는 상담 시간
+
+		public string Content { get; set; } // 상담 내용
+
+		public int State { get; set; } // 청원 상태 - 0: 등록됨, 1: 승인, 2: 거절
+
+		public string Code { get; set; } // 사용자 식별 번호
+
+		public string Reason { get; set; } // 반려 사유 - 반려 시
+	}
+}
