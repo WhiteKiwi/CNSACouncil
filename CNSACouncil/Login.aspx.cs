121d8e280fa2df48d8913ebd47209612a9d619d8
장지훈 <s5v7@outlook.kr>
2018-07-23 16:23:19 +0900

    로그인, 로그아웃 페이지 추가

---
+++ CNSACouncil/Login.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CNSACouncil {
+	public partial class Login : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+
+		}
+		// TODO: CNSAnet ID로 로그인
+	}
+}
---
+++ CNSACouncil/Logout.aspx.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CNSACouncil {
+	public partial class Logout : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			// TODO: 로그인 세션 삭제
+		}
+	}
+}
