bf8513edf27439cafd93cc1efe45a1e847a6667b
장지훈 <s5v7@outlook.kr>
2018-07-25 21:25:02 +0900

    건의 확인 페이지 추가
    POST로 SuggestionID 값을 보내주면 해당하는 건의 지난 건의로 이동

---
+++ CNSACouncil/admin/SuggestionCheck.aspx.cs
@@ -0,0 +1,14 @@
+﻿using CNSACouncil.Managers;
+using System;
+
+namespace CNSACouncil.admin {
+	public partial class SuggestionCheck : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Request.QueryString["SuggestionID"] != null) {
+				SuggestionManager.SuggestionCheck(Request.QueryString["SuggestionID"]);
+			}
+
+			Response.Redirect("/admin/SuggestionManagement.aspx");
+		}
+	}
+}
