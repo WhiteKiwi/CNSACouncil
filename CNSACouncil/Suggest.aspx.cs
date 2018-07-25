8cb4c1186c6699c435de2146eb6c4de3662479f1
장지훈 <s5v7@outlook.kr>
2018-07-25 11:18:43 +0900

    건의 페이지 알림
    제목이 없을 경우
    내용이 없을 경우
    건의에 실패했을 경우

--- CNSACouncil/Suggest.aspx.cs
+++ CNSACouncil/Suggest.aspx.cs
@@ -9,16 +9,24 @@ namespace CNSACouncil {
 		protected void SuggestButton_Click(object sender, EventArgs e) {
 			SuggestButton.Enabled = false;
 
-			// 건의 추가
-			Managers.SuggestionManager.UploadSuggestion(new Models.Suggestion {
-				Title = SuggestionTitle.Text,
-				Content = Content.Text,
-				IP = Request.UserHostAddress,
-				SuggestAt = DateTime.Now.AddHours(9)
-			});
-			// TODO: 건의 성공, 건의 실패 판별
+			if (string.IsNullOrEmpty(SuggestionTitle.Text.Trim())) // 제목이 비어있을 경우
+				Response.Write("<script>alert('제목을 작성해주세요.');</script>");
+			else if (string.IsNullOrEmpty(Content.Text.Trim())) // 내용이 비어 있을 경우
+				Response.Write("<script>alert('내용을 작성해주세요.');</script>");
+			else {
+				// 건의 추가
+				if (Managers.SuggestionManager.UploadSuggestion(new Models.Suggestion {
+					Title = SuggestionTitle.Text,
+					Content = Content.Text,
+					IP = Request.UserHostAddress,
+					SuggestAt = DateTime.Now.AddHours(9)
+				}) == 0)
+					Response.Write("<script>alert('건의에 실패했습니다. 다시 시도해주세요.');</script>");
+				else
+					Response.Redirect("/Suggest.aspx");
+			}
 
-			Response.Redirect("/Suggest.aspx");
+			SuggestButton.Enabled = true;
 		}
 	}
 }
