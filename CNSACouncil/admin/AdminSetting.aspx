92847825a8dfc9b2dba4c76e93903dc775da82ab
장지훈 <s5v7@outlook.kr>
2018-07-26 14:24:17 +0900

    GET요청으로 계정 설정하는 페이지 추가

---
+++ CNSACouncil/admin/AdminSetting.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdminSetting.aspx.cs" Inherits="CNSACouncil.admin.AdminSetting" %>
---
+++ CNSACouncil/admin/AdminSetting.aspx.cs
@@ -0,0 +1,20 @@
+﻿using CNSACouncil.Managers;
+using System;
+
+namespace CNSACouncil.admin {
+	public partial class AdminSetting : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Request.QueryString["select"] != null && Request.QueryString["adminID"] != null) {
+				if (Request.QueryString["select"] == "reset") {
+					// 계정 비밀번호 초기화
+					AdminManager.ResetPassword(Request.QueryString["adminID"]);
+				} else if (Request.QueryString["select"] == "delete") {
+					// 계정 삭제
+					AdminManager.DeleteAccount(Request.QueryString["adminID"]);
+				}
+			}
+
+			Response.Redirect("/admin/AdminManagement.aspx");
+		}
+	}
+}
