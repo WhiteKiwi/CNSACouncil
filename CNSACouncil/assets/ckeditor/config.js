6fc90ef4c362fbaeda110046721f5707220bddf7
장지훈 <s5v7@outlook.kr>
2018-07-28 18:29:26 +0900

    CKEditor 버전 변경
    CKEditor -> CKEditor for ASP.NET

--- CNSACouncil/assets/ckeditor/config.js
+++ CNSACouncil/assets/ckeditor/config.js
@@ -1,10 +1,8 @@
-/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+﻿/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+    config.enterMode = CKEDITOR.ENTER_BR;
 };
