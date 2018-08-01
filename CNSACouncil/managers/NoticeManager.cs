811f1edbd33753abdfdf6ad27a678de4cc20d184
장지훈 <s5v7@outlook.kr>
2018-08-01 13:52:13 +0900

    페이지에 해당하는 공지들을 반환하는 함수

--- CNSACouncil/managers/NoticeManager.cs
+++ CNSACouncil/managers/NoticeManager.cs
@@ -1,5 +1,7 @@
 ﻿using CNSACouncil.Models;
 using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 
 namespace CNSACouncil.Managers {
 	public static class NoticeManager {
@@ -38,6 +40,41 @@ namespace CNSACouncil.Managers {
 
 				return result;
 			}
+		}
+
+		/// <summary>
+		/// 페이지에 해당하는 공지들을 반환하는 함수
+		/// </summary>
+		/// <param name="page">요청한 페이지 번호</param>  
+		/// <see cref="Notice"/>
+		public static List<Notice> GetNoticeByPage(int page) {
+			var result = new List<Notice>();
+
+			// Connect to DB;
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["COUNCILDB"].ConnectionString)) {
+				conn.Open();
+
+				// Get Notice Count
+				string sql = "SELECT count(*) FROM " + NOTICES + ";";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+				int NoticeCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+				// Get Notice
+				sql = "SELECT * FROM " + NOTICES + " ORDER BY ID DESC LIMIT 10 OFFSET " + ((page - 1) * 10) + ";";
+				cmd.CommandText = sql;
+
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					result.Add(new Notice {
+						ID = (int)rdr["ID"],
+						Title = (string)rdr["Title"],
+						Content = (string)rdr["Content"],
+						NoticeAt = (DateTime)rdr["NoticeAt"]
+					});
+				}
+
+			}
+			return result;
 		}
 	}
 }
