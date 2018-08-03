39645e692bf190914531a7dc2b243ef0395df06a
장지훈 <s5v7@outlook.kr>
2018-08-03 16:34:15 +0900

    id와 마감일 form으로 받아 사업을 완료시키는 페이지 추가

---
+++ CNSACouncil/admin/ProjectCheck.aspx.cs
@@ -0,0 +1,14 @@
+﻿using CNSACouncil.Managers;
+using System;
+
+namespace CNSACouncil.admin {
+	public partial class ProjectCheck : System.Web.UI.Page {
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Request.Form["id"] != null) {
+				ProjectManager.ProjectCheck(Request.Form["id"], Request.Form["endAt"]);
+			}
+
+			Response.Redirect("/admin/ProjectManagement.aspx");
+		}
+	}
+}
