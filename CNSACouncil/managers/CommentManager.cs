dc03b2c9f3ccb3d1662f01fcd173ccf2a654293f
장지훈 <s5v7@outlook.kr>
2018-08-15 19:52:29 +0900

    Comment Manager 추가

---
+++ CNSACouncil/managers/CommentManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using CNSACouncil.Models;
+using MySql.Data.MySqlClient;
+
+namespace CNSACouncil.Managers {
+	public static class CommentManager {
+		// Table Name
+		private const string COMMENTS = "comments";
+
+		}
+	}
+}
