8b497d26134640c750a6348a1384a1edf4399ef1
장지훈 <s5v7@outlook.kr>
2018-08-15 21:30:42 +0900

    댓글을 가져오는 메서드 추가

--- CNSACouncil/managers/CommentManager.cs
+++ CNSACouncil/managers/CommentManager.cs
@@ -1,5 +1,7 @@
 ﻿using CNSACouncil.Models;
 using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
 
 namespace CNSACouncil.Managers {
 	public static class CommentManager {
@@ -31,6 +33,37 @@ namespace CNSACouncil.Managers {
 				// Connection Close
 				conn.Close();
 			}
+		}
+
+		/// <summary>
+		/// 글 번호, 분류에 해당하는 댓글을 반환하는 메서드
+		/// </summary>
+		/// <param name="id">요청한 글 번호</param>  
+		/// <param name="classification">요청한 분류</param>  
+		/// <see cref="Comment"/>
+		public static List<Comment> GetComments(int id, int classification) {
+			var result = new List<Comment>();
+
+			// Connect to DB;
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["COUNCILDB"].ConnectionString)) {
+				conn.Open();
+
+				// Get Comments
+				string sql = "SELECT* FROM " + COMMENTS + " WHERE Classification='" + classification + "' AND PostID='" + id + "';";
+				MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+				var rdr = cmd.ExecuteReader();
+				while (rdr.Read()) {
+					result.Add(new Comment {
+						PostID = (int)rdr["PostID"],
+						Content = (string)rdr["Content"],
+						WriterID = (string)rdr["WriterID"],
+						CommentAt = (DateTime)rdr["CommentAt"],
+						Classification = (int)rdr["Classification"]
+					});
+				}
+			}
+			return result;
 		}
 	}
 }
