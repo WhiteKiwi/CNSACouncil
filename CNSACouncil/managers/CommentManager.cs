60a5a1e7e5fa541cd80ac504bc81f21adae00368
장지훈 <s5v7@outlook.kr>
2018-08-15 21:15:51 +0900

    댓글을 추가하는 메서드 추가

--- CNSACouncil/managers/CommentManager.cs
+++ CNSACouncil/managers/CommentManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using CNSACouncil.Models;
+﻿using CNSACouncil.Models;
 using MySql.Data.MySqlClient;
 
 namespace CNSACouncil.Managers {
@@ -8,5 +6,31 @@ namespace CNSACouncil.Managers {
 		// Table Name
 		private const string COMMENTS = "comments";
 
+		/// <summary>
+		/// 댓글을 추가하는 메서드
+		/// </summary>
+		/// <param name="comment">Comment class in Models</param>  
+		/// <see cref="Comment"/>
+		public static void AddComment(Comment comment) {
+			// Connect to DB
+			using (var conn = new MySqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["COUNCILDB"].ConnectionString)) {
+				conn.Open();
+
+				// Command Text - Create
+				string commandText = "INSERT INTO " + COMMENTS + "(PostID, Content, WriterID, CommentAt, Classification) VALUES (?, ?, ?, ?, ?);";
+				var cmd = new MySqlCommand(commandText, conn);
+				cmd.Parameters.Add("PostID", MySqlDbType.Int32).Value = comment.PostID;
+				cmd.Parameters.Add("Content", MySqlDbType.VarChar).Value = comment.Content;
+				cmd.Parameters.Add("WriterID", MySqlDbType.VarChar).Value = comment.WriterID;
+				cmd.Parameters.Add("CommentAt", MySqlDbType.DateTime).Value = comment.CommentAt;
+				cmd.Parameters.Add("Classification", MySqlDbType.Int32).Value = comment.Classification;
+
+				// The number of rows affected
+				cmd.ExecuteNonQuery();
+
+				// Connection Close
+				conn.Close();
+			}
+		}
 	}
 }
