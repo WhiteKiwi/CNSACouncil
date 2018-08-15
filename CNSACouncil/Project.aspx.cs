ebe85e6d7d515ae45b27fee67c50887b782bec29
장지훈 <s5v7@outlook.kr>
2018-08-15 21:49:22 +0900

    사업보기 페이지 댓글 추가

--- CNSACouncil/Project.aspx.cs
+++ CNSACouncil/Project.aspx.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+﻿using CNSACouncil.Managers;
+using System;
 
 namespace CNSACouncil {
 	public partial class AProject : System.Web.UI.Page {
 		protected void Page_Load(object sender, EventArgs e) {
 
+		}
+
+		protected void CommentButton_Click(object sender, EventArgs e) {
+			if (string.IsNullOrEmpty(Content.Text.Trim())) {
+				Response.Write("<script>alert('내용을 입력해주세요.');</script>");
+			} else if (Session["UserID"] == null) {
+				// 로그인 페이지로 Redirect
+				Response.Redirect("/Login.aspx?classification=2&state=" + Request.QueryString["state"] + "&id=" + Request.QueryString["id"]);
+			} else {
+				CommentButton.Enabled = false;
+
+				CommentManager.AddComment(new Models.Comment {
+					PostID = int.Parse(Request.QueryString["id"]),
+					Content = Content.Text,
+					WriterID = (string)Session["UserID"],
+					CommentAt = DateTime.Now.AddHours(9),
+					Classification = 2
+				});
+
+				CommentButton.Enabled = true;
+
+				Response.Redirect(Request.RawUrl);
+			}
 		}
 	}
 }
